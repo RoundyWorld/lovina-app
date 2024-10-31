aba396b961b510aad75031cdac1c12e69ce0f5a4
lovina <lovina@rasa-legal.com>
2024-10-30 22:01:26 -0700

    more updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import ResponsiveAppBar from './components/navbar';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+
 import Resume from './pages/resume';
 import Games from './pages/games';
 import Hobbies from './pages/hobbies';
@@ -13,26 +14,24 @@ function App() {
   const [selectedPage, setSelectedPage] = useState(pages[0]);
   return (
     <BrowserRouter>
-    <div className="App">
-      
-      
-      <header className="App-header">
-        <ResponsiveAppBar 
-          pages={pages}
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-        />
-      </header>
-      <div>
-        <Routes>
-          <Route path="/" element={<Resume />} />
-          <Route path="*" element={<Resume />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/hobbies" element={<Hobbies />} />
-        </Routes>
+      <div className="App">
+        <header className="App-header">
+          <ResponsiveAppBar 
+            pages={pages}
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        </header>
+        <div>
+          <Routes>
+            <Route path="/" element={<Resume />} />
+            <Route path="*" element={<Resume />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/hobbies" element={<Hobbies />} />
+          </Routes>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 }
